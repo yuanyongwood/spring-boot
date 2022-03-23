1db97b6b0bc5abd216f9a80e2fc03534ea2d8d91
吴元泳 <yuanyongwood@163.com>
2022-03-23 11:32:36 +0800

    init

---
+++ comment-source-code/src/main/java/com/source/code/Application.java
@@ -0,0 +1,9 @@
+package com.source.code;
+
+/**
+ * @author wuyuanyong
+ * When I wrote this code, only God and I understood what I was doing
+ * Now, God only knows
+ */
+public class Application {
+}
